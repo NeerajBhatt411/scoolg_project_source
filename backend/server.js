cb7e13234e4d27bbd7154c049b244f3d59cbad2b
neerajbhatt_15 <neeraj.bhatt@ongraph.com>
2026-05-10 23:00:14 +0530

    fix: remove unused __filename and __dirname to fix Netlify syntax error

--- backend/server.js
+++ backend/server.js
@@ -17,8 +17,6 @@ import Student from './models/Student.js';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 dotenv.config();
 
