a699e52f6a8e24922ed9fec6bfe8a65b9fdc61b5
NeerajBhatt411 <neerajbhattadx@gmail.com>
2026-06-13 23:48:41 +0530

    Fix FOUC, set up PWA icons, remove school name from teacher sidebar

--- scoolg-student-pwa/vite.config.js
+++ scoolg-student-pwa/vite.config.js
@@ -15,16 +15,9 @@ export default defineConfig({
         description: 'ScoolG Student App',
         theme_color: '#f8fafc',
         icons: [
-          {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
+          { src: 'pwa-192x192.png', sizes: '192x192', type: 'image/png', purpose: 'any' },
+          { src: 'pwa-512x512.png', sizes: '512x512', type: 'image/png', purpose: 'any' },
+          { src: 'pwa-512x512.png', sizes: '512x512', type: 'image/png', purpose: 'maskable' },
         ]
       },
       devOptions: {
